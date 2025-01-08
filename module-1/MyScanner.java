75e770b67554081cfa3bd35514cea4856f2e0674
AntonPDC <antonpdecesare@gmail.com>
2025-01-08 10:31:00 -0500

    module 1 start

---
+++ module-1/MyScanner.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args){
+        try (Scanner myObj = new Scanner(System.in)) {
+            System.out.println("Enter amount of water in kilograms: ");
+            double water = myObj.nextDouble();
+            System.out.println("Enter initial temperature in Celsius: ");
+            double initialTemperature = myObj.nextDouble();
+            System.out.println("Enter final temperature in Celsius: ");
+            double finalTemperature = myObj.nextDouble();
+            System.out.println((water * (initialTemperature + finalTemperature)) * 4184);
+        }
+    }
+}
