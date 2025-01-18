c8d035123981c5a3143b7c94fd333a0df0d9ed92
AntonPDC <antonpdecesare@gmail.com>
2025-01-18 15:05:33 -0500

    fix

--- module-2/DeCesare_mod2.2.java
+++ module-2/DeCesare_mod2.2.java
@@ -3,13 +3,15 @@ import java.util.Scanner;
 class Main{
     public static void main(String[] args) {
         // assign a variable to a random integer between 1 and 3 inclusively
-        int computerSign = (int) (Math.random()*4);
+        int computerSign = 1 + (int) (Math.random() * 3);
 
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your sign (1: rock, 2: paper, 3: scissors): ");
-        int userSign = scanner.nextInt();
-        scanner.close();
+
+        int userSign;
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Enter your sign (1: rock, 2: paper, 3: scissors): ");
+            userSign = scanner.nextInt();
+        }
         System.out.println(computerSign);
         System.out.println(userSign);
 
