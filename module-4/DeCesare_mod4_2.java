699edaea3e3c2cbd9a42a750f6c2f3f0f6febac8
AntonPDC <antonpdecesare@gmail.com>
2025-01-26 11:44:52 -0500

    mod4 final

--- module-4/DeCesare_mod4_2.java
+++ module-4/DeCesare_mod4_2.java
@@ -1,19 +1,31 @@
-class Main{
-    public static void main(String[] args) {
-        short[] shortArray = {12345, 23456, 3276, 7890, 12034}; // Random "short" values
-        int[] numbers = {1,2,3,4,5,6,7,8}; // Random "int" values
-        long[] longArray = {987654321L, 123456789L, -2345678901L, 3456789012L, -9876543210L,
-                    2345678901L, 7654321098L, 9876543212L, -1234567890L}; //Random "long" values
-        double[] doubleArray = {12.34, 56.78, 90.12, 34.56, 78.90, 21.43, 65.87}; // Random "double" values
 
-        System.out.println("Short Average: " + average(shortArray) + "\t|\t" + displayArray(shortArray)+  "\n");
-        System.out.println("Integer Average: " + average(numbers) + "\t|\t" + displayArray(numbers)+  "\n");
-        System.out.println("Long Average: " + average(longArray) + "\t|\t" +displayArray(longArray) +  "\n");
-        System.out.println("Double Average: " + average(doubleArray) + "\t|\t" +displayArray(doubleArray) +  "\n");
+/*
+ * Anton DeCesare Mod 4.2
+ * This program takes arrays of different data types and calculates the average, while only calling "one" method.
+ * The key to this is using overloading methods that are created to handle each data type as the prompt.
+ */
+
+
+public class DeCesare_mod4_2{
+    public static void main(String[] args) {
+        // Random "short" values
+        short[] shortArray = {12345, 23456, 3276, 7890, 12034};
+        // Random "int" values
+        int[] numbers = {1,2,3,4,5,6,7,8};
+        //Random "long" values
+        long[] longArray = {987654321L, 123456789L, -2345678901L, 3456789012L, -9876543210L,
+                    2345678901L, 7654321098L, 9876543212L, -1234567890L};
+        // Random "double" values
+        double[] doubleArray = {12.34, 56.78, 90.12, 34.56, 78.90, 21.43, 65.87};
+        // Calling "average" method to calculate the average of each value in the array, and then display the array as well.
+        System.out.println("Short Average: " + average(shortArray) + "\t| " + displayArray(shortArray)+  "\n");
+        System.out.println("Integer Average: " + average(numbers) + "\t| " + displayArray(numbers)+  "\n");
+        System.out.println("Long Average: " + average(longArray) + "\t| " +displayArray(longArray) +  "\n");
+        System.out.println("Double Average: " + average(doubleArray) + "\t| " +displayArray(doubleArray) +  "\n");
 
 
     }
-
+    // Calculates the average of an array of shorts
     public static short average(short[] array){
         int sum = 0;
         for(int i = 0; i < array.length; i++){
@@ -21,6 +33,8 @@ class Main{
         }
         return (short)(sum / array.length);
     }
+
+    // Calculates the average of an array of ints
     public static int average(int[] array){
         int sum = 0;
         for(int i = 0; i < array.length; i++){
@@ -29,6 +43,7 @@ class Main{
         return (int)sum / array.length;
     }
 
+    // Calculates the average of an array of longs
     public static long average(long[] array){
         long sum = 0;
         for(int i = 0; i < array.length; i++){
@@ -37,6 +52,7 @@ class Main{
         return sum / array.length;
     }
 
+    // Calculates the average of an array of doubles
     public static Double average(double[] array){
         double sum = 0;
         for(int i = 0; i < array.length; i++){
@@ -45,6 +61,7 @@ class Main{
         return sum / array.length;
     }
 
+    // Displays the shorts array as a STRING
     public static String displayArray(short[] array){
         String result = "[";
         for(int i = 0; i < array.length; i++){
@@ -54,31 +71,40 @@ class Main{
         result += "]";
         return result;
     }
+
+
+    // Displays the ints array as a STRING
     public static String displayArray(int[] array){
-        String result = "[";
+        String result = "{";
         for(int i = 0; i < array.length; i++){
             result += array[i];
             if(i < array.length - 1) result += ", ";
         }
-        result += "]";
+        result += "}";
         return result;
     }
+
+
+    // Displays the longs array as a STRING
     public static String displayArray(long[] array){
-        String result = "[";
+        String result = "{";
         for(int i = 0; i < array.length; i++){
             result += array[i];
             if(i < array.length - 1) result += ", ";
         }
-        result += "]";
+        result += "}";
         return result;
     }
+
+
+    // Displays the doubles array as a STRING
     public static String displayArray(double[] array){
-        String result = "[";
+        String result = "{";
         for(int i = 0; i < array.length; i++){
             result += array[i];
             if(i < array.length - 1) result += ", ";
         }
-        result += "]";
+        result += "}";
         return result;
     }
 
